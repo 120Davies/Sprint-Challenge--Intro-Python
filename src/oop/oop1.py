f93952252a960a7afd7526e7cee657bac388b68d
Ro McKay Davies <50719703+120Davies@users.noreply.github.com>
2020-04-10 10:58:12 -0600

    Non-stretch goals met

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,26 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle():                # This is the base class,
+    pass                        # the class from which the others inherit
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+# check
